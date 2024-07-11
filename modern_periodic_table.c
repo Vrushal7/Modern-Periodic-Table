8c74f16f15d0925b23c5c13d210b3bdeb0071ff2
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-07-11 21:23:56 +0530

    Commit-2

--- modern_periodic_table.c
+++ modern_periodic_table.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+int element_info(num)
+{
+  return 0;
+}
+int main()
+{
+    int choice;
+    while (1) {
+        printf("Enter the atomic number of an element: ");
+        if (scanf("%d", &choice) != 1) 
+        {
+            while (getchar() != '\n');
+            printf("Invalid input. Please enter a valid number.\n");
+            continue;
+        }
+
+        if (choice<=118 && choice>=0) {
+            element_info(choice);
+            break; 
+        } else {
+            printf("Elements in Modern periodic table are upto 118.\nEnter a valid atomic number!\n");
+        }
+    }
+
+        return 0;
+
+
+}
