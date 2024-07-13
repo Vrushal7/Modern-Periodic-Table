2b13a2ce3e4afc919499bc6b135bae736a96cb40
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-07-13 21:38:41 +0530

    new commit

--- modern_periodic_table.c
+++ modern_periodic_table.c
@@ -1,8 +1,371 @@
 #include <stdio.h>
-int element_info(num)
+int element_info(int num)
 {
+    switch (num)
+    {
+    case 1: 
+        printf("\nHydrogen\n"); 
+        break;
+    case 2: 
+        printf("\nHelium\n"); 
+        break;
+    case 3: 
+        printf("\nLithium\n"); 
+        break;
+    case 4: 
+        printf("\nBeryllium\n"); 
+        break;
+    case 5: 
+        printf("\nBoron\n"); 
+        break;
+    case 6: 
+        printf("\nCarbon\n"); 
+        break;
+    case 7: 
+        printf("\nNitrogen\n"); 
+        break;
+    case 8: 
+        printf("\nOxygen\n"); 
+        break;
+    case 9: 
+        printf("\nFluorine\n"); 
+        break;
+    case 10: 
+        printf("\nNeon\n"); 
+        break;
+    case 11:
+        printf("\nSodium\n");
+        break;
+    case 12:
+        printf("\nMagnesium\n");
+        break;
+    case 13:
+        printf("\nAluminum\n");
+        break;
+    case 14:
+        printf("\nSilicon\n");
+        break;
+    case 15:
+        printf("\nPhosphorus\n");
+        break;
+    case 16:
+        printf("\nSulfur\n");
+        break;
+    case 17:
+        printf("\nChlorine\n");
+        break;
+    case 18:
+        printf("\nArgon\n");
+        break;
+    case 19:
+        printf("\nPotassium\n");
+        break;
+    case 20:
+        printf("\nCalcium\n");
+        break;
+    case 21:
+        printf("\nScandium\n");
+        break;
+    case 22:
+        printf("\nTitanium\n");
+        break;
+    case 23:
+        printf("\nVanadium\n");
+        break;
+    case 24:
+        printf("\nChromium\n");
+        break;
+    case 25:
+        printf("\nManganese\n");
+        break;
+    case 26:
+        printf("\nIron\n");
+        break;
+    case 27:
+        printf("\nCobalt\n");
+        break;
+    case 28:
+        printf("\nNickel\n");
+        break;
+    case 29:
+        printf("\nCopper\n");
+        break;
+    case 30:
+        printf("\nZinc\n");
+        break;
+    case 31:
+        printf("\nGallium\n");
+        break;
+    case 32:
+        printf("\nGermanium\n");
+        break;
+    case 33:
+        printf("\nArsenic\n");
+        break;
+    case 34:
+        printf("\nSelenium\n");
+        break;
+    case 35:
+        printf("\nBromine\n");
+        break;
+    case 36:
+        printf("\nKrypton\n");
+        break;
+    case 37:
+        printf("\nRubidium\n");
+        break;
+    case 38:
+        printf("\nStrontium\n");
+        break;
+    case 39:
+        printf("\nYttrium\n");
+        break;
+    case 40:
+        printf("\nZirconium\n");
+        break;
+    case 41:
+        printf("\nNiobium\n");
+        break;
+    case 42:
+        printf("\nMolybdenum\n");
+        break;
+    case 43:
+        printf("\nTechnetium\n");
+        break;
+    case 44:
+        printf("\nRuthenium\n");
+        break;
+    case 45:
+        printf("\nRhodium\n");
+        break;
+    case 46:
+        printf("\nPalladium\n");
+        break;
+    case 47:
+        printf("\nSilver\n");
+        break;
+    case 48:
+        printf("\nCadmium\n");
+        break;
+    case 49:
+        printf("\nIndium\n");
+        break;
+    case 50:
+        printf("\nTin\n");
+        break;
+    case 51:
+        printf("\nAntimony\n");
+        break;
+    case 52:
+        printf("\nTellurium\n");
+        break;
+    case 53:
+        printf("\nIodine\n");
+        break;
+    case 54:
+        printf("\nXenon\n");
+        break;
+    case 55:
+        printf("\nCesium\n");
+        break;
+    case 56:
+        printf("\nBarium\n");
+        break;
+    case 57:
+        printf("\nLanthanum\n");
+        break;
+    case 58:
+        printf("\nCerium\n");
+        break;
+    case 59:
+        printf("\nPraseodymium\n");
+        break;
+    case 60:
+        printf("\nNeodymium\n");
+        break;
+    case 61:
+        printf("\nPromethium\n");
+        break;
+    case 62:
+        printf("\nSamarium\n");
+        break;
+    case 63:
+        printf("\nEuropium\n");
+        break;
+    case 64:
+        printf("\nGadolinium\n");
+        break;
+    case 65:
+        printf("\nTerbium\n");
+        break;
+    case 66:
+        printf("\nDysprosium\n");
+        break;
+    case 67:
+        printf("\nHolmium\n");
+        break;
+    case 68:
+        printf("\nErbium\n");
+        break;
+    case 69:
+        printf("\nThulium\n");
+        break;
+    case 70:
+        printf("\nYtterbium\n");
+        break;
+    case 71:
+        printf("\nLutetium\n");
+        break;
+    case 72:
+        printf("\nHafnium\n");
+        break;
+    case 73:
+        printf("\nTantalum\n");
+        break;
+    case 74:
+        printf("\nTungsten\n");
+        break;
+    case 75:
+        printf("\nRhenium\n");
+        break;
+    case 76:
+        printf("\nOsmium\n");
+        break;
+    case 77:
+        printf("\nIridium\n");
+        break;
+    case 78:
+        printf("\nPlatinum\n");
+        break;
+    case 79:
+        printf("\nGold\n");
+        break;
+    case 80:
+        printf("\nMercury\n");
+        break;
+    case 81:
+        printf("\nThallium\n");
+        break;
+    case 82:
+        printf("\nLead\n");
+        break;
+    case 83:
+        printf("\nBismuth\n");
+        break;
+    case 84:
+        printf("\nPolonium\n");
+        break;
+    case 85:
+        printf("\nAstatine\n");
+        break;
+    case 86:
+        printf("\nRadon\n");
+        break;
+    case 87:
+        printf("\nFrancium\n");
+        break;
+    case 88:
+        printf("\nRadium\n");
+        break;
+    case 89:
+        printf("\nActinium\n");
+        break;
+    case 90:
+        printf("\nThorium\n");
+        break;
+    case 91:
+        printf("\nProtactinium\n");
+        break;
+    case 92:
+        printf("\nUranium\n");
+        break;
+    case 93:
+        printf("\nNeptunium\n");
+        break;
+    case 94:
+        printf("\nPlutonium\n");
+        break;
+    case 95:
+        printf("\nAmericium\n");
+        break;
+    case 96:
+        printf("\nCurium\n");
+        break;
+    case 97:
+        printf("\nBerkelium\n");
+        break;
+    case 98:
+        printf("\nCalifornium\n");
+        break;
+    case 99:
+        printf("\nEinsteinium\n");
+        break;
+    case 100:
+        printf("\nFermium\n");
+        break;
+    case 101:
+        printf("\nMendelevium\n");
+        break;
+    case 102:
+        printf("\nNobelium\n");
+        break;
+    case 103:
+        printf("\nLawrencium\n");
+        break;
+    case 104:
+        printf("\nRutherfordium\n");
+        break;
+    case 105:
+        printf("\nDubnium\n");
+        break;
+    case 106:
+        printf("\nSeaborgium\n");
+        break;
+    case 107:
+        printf("\nBohrium\n");
+        break;
+    case 108:
+        printf("\nHassium\n");
+        break;
+    case 109:
+        printf("\nMeitnerium\n");
+        break;
+    case 110:
+        printf("\nDarmstadtium\n");
+        break;
+    case 111:
+        printf("\nRoentgenium\n");
+        break;
+    case 112:
+        printf("\nCopernicium\n");
+        break;
+    case 113:
+        printf("\nNihonium\n");
+        break;
+    case 114:
+        printf("\nFlerovium\n");
+        break;
+    case 115:
+        printf("\nMoscovium\n");
+        break;
+    case 116:
+        printf("\nLivermorium\n");
+        break;
+    case 117:
+        printf("\nTennessine\n");
+        break;
+    case 118:
+        printf("\nOganesson\n");
+        break;
+    
+    default:
+        break;
+    }
+  
+  
   return 0;
 }
+
 int main()
 {
     int choice;
